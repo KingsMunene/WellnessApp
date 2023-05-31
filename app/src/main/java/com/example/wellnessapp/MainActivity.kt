18938154a00aabab58009c0573a5cdafce462c14
KingsMunene <munenekings2@gmail.com>
2023-05-31 15:57:38 +0300

    Pass the list and the remove function to the main activity. Modification of the task class to store the check box value

--- app/src/main/java/com/example/wellnessapp/MainActivity.kt
+++ app/src/main/java/com/example/wellnessapp/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import com.example.wellnessapp.ui.theme.StateFullTaskItem
 import com.example.wellnessapp.ui.theme.WellnessAppTheme
 
@@ -43,12 +44,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WellnessScreen() {
+fun WellnessScreen(wellnessViewModel: WellnessViewModel = viewModel()) {
     Column {
         StateFullCounter()
 
-        val list = remember { getWellnessList().toMutableStateList() }
-        TaskList(taskList = list, onClicked = {task -> list.remove(task) })
+        TaskList(taskList = wellnessViewModel.tasks,
+            onClicked = {task -> wellnessViewModel.remove(task) })
     }
 }
 
--- app/src/main/java/com/example/wellnessapp/TaskClass.kt
+++ app/src/main/java/com/example/wellnessapp/TaskClass.kt
@@ -1,4 +1,4 @@
 package com.example.wellnessapp
 
-data class TaskClass(val taskID : Int, val taskName : String)
+data class TaskClass(val taskID : Int, val taskName : String, var checked : Boolean = false)
 
