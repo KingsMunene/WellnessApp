689996f42601be08910e6716f00798aaaabd60f1
KingsMunene <munenekings2@gmail.com>
2023-06-02 12:44:23 +0300

    implementing the view model on the main activity

--- app/src/main/java/com/example/wellnessapp/MainActivity.kt
+++ app/src/main/java/com/example/wellnessapp/MainActivity.kt
@@ -14,16 +14,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.toMutableStateList
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import com.example.wellnessapp.ui.theme.StateFullTaskItem
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 import com.example.wellnessapp.ui.theme.WellnessAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,7 +49,9 @@ fun WellnessScreen(wellnessViewModel: WellnessViewModel = viewModel()) {
         StateFullCounter()
 
         TaskList(taskList = wellnessViewModel.tasks,
-            onClicked = {task -> wellnessViewModel.remove(task) })
+            onClicked = {task -> wellnessViewModel.remove(task) },
+            onCheckChanged = {taskClass, checked -> wellnessViewModel.changeTaskChecked(taskClass, checked)}
+            )
     }
 }
 
--- app/src/main/java/com/example/wellnessapp/WellnessViewModel.kt
+++ app/src/main/java/com/example/wellnessapp/WellnessViewModel.kt
@@ -3,6 +3,7 @@ package com.example.wellnessapp
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 
+
 class WellnessViewModel : ViewModel() {
 
     private val _tasks = getWellnessList().toMutableStateList()
